eb2cfec4547b6222cc34e2cdbe390c0a82759ba1
echedgy <35992036+echedgy@users.noreply.github.com>
2019-09-02 22:14:24 -0500

    Add files via upload

---
+++ 2.2 Madlibs.py
@@ -0,0 +1,29 @@
+# variables for printing program
+action = input("Enter a verb")
+example = input("Enter a noun")
+action2 = input("Enter another verb")
+body_part = input("Enter a body part")
+food = "Enter a food"
+color = "Enter a color"
+emotion = "Enter an emotion"
+action3 = "Enter one last verb"
+
+print(action + "sesame (We've places to go)")
+print("We've people to see (Let's put 'em on hold)")
+print("There's all sorts of" + example + "that I bet you can make")
+print("When you had to" + action2 + ", say the word. \n")
+
+print("Well I know that getting you alone isn't easy to do")
+print("But with the exception of you I dislike everyone in the room")
+print("And I don't wanna lie but I don't wanna tell you the truth")
+print("Get the sense that you're on the move and you'll probably be leaving soon \n")
+
+print(body_part + "the color of")
+print(color + "and" + food + "dust")
+print("It's a" + emotion + "thing that I cannot explain")
+print("Don't you know the train keeps rolling \n")
+
+print("Well I know that getting you alone isn't easy to do")
+print("And I don't wanna" + action3 + "and I don't wanna tell you the truth")
+print("And I know we got places to go, we got people to see")
+print("Think we both ought put 'em on hold and I know you agree")
