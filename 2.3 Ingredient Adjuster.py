83d27053f7b8b762e0fb1c3e33c8be835e6b6873
echedgy <35992036+echedgy@users.noreply.github.com>
2019-09-03 23:49:08 -0500

    Add files via upload

--- 2.3 Ingredient Adjuster.py
+++ 2.3 Ingredient Adjuster.py
@@ -5,14 +5,16 @@
 # The recipe produces 48 cookies with this amount of ingredients. Write a program that asks the user
 # how many cookies they want to make, and then displays the number of cups
 # (to two decimal places) of each ingredient needed for the specified number of cookies.
-sugar = 1.5
-butter = 1
-flour = 2.75
-cookie_num = int(input('How many cookies do you want to make?'))
-sugar = (input('How much sugar do you have'))
-butter = float(input('How much butter do you have'))
-flour = float(input('How much flour do you have'))
+COOKIE_NUMBER = 48
 
-print("You wil need: " + format(sugar * cookie_num, ', .2f ') + "cups of sugar")
-print("you will need:" + format(butter * cookie_num, ', .2f') + "cups of butter")
-print("you will need:" + format(flour * cookie_num, ', .2f') + "cups of flour")
+cupsOfSugar_cookie_num = 1.5/COOKIE_NUMBER
+cupsOfButter_cookie_num = 1/COOKIE_NUMBER
+cupsOfFlour_cookie_num = 2.75/COOKIE_NUMBER
+userNumber_of_cookies = int(input('How many cookies do you want to make? '))
+print("For " + str(userNumber_of_cookies) + " cookies, you will need")
+
+expectedCupsOfSugar = userNumber_of_cookies * cupsOfSugar_cookie_num
+expectedCupsOfButter = userNumber_of_cookies * cupsOfButter_cookie_num
+expectedCupsOfFlour = userNumber_of_cookies * cupsOfFlour_cookie_num
+
+print(format(expectedCupsOfSugar, ".2f") + " cups of sugar, " + format(expectedCupsOfButter, ".2f") + " cups of butter and " + format(expectedCupsOfFlour, ".2f") + " cups of flour")
