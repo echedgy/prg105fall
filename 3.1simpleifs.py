a0c62079b18e00c55cafcda1237d8a78d8f39508
echedgy <35992036+echedgy@users.noreply.github.com>
2019-09-05 13:12:54 -0500

    Add files via upload

---
+++ 3.1simpleifs.py
@@ -0,0 +1,17 @@
+user_number = int(input("enter a number from 1 to 7"))
+if user_number == 1:
+    print("monday")
+elif user_number == 2:
+    print("Tuesday")
+elif user_number == 3:
+    print("Wednesday")
+elif user_number == 4:
+    print("Thursday")
+elif user_number == 5:
+    print("Friday")
+elif user_number == 6:
+    print("Saturday")
+elif user_number == 7:
+    print("Sunday")
+else:
+    print("error")
