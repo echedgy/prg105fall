233faefd4ef7eea1862ce2259f60c62e40aba7c2
echedgy <35992036+echedgy@users.noreply.github.com>
2019-09-11 00:27:21 -0500

    Add files via upload

--- 3.3 Nested Ifs.py
+++ 3.3 Nested Ifs.py
@@ -1,5 +1,26 @@
-int(input("Which package are you currently using?"))
-if package == "A":
+package = input("Which package do you have? (A, B, C): ")
+print("You entered Package " + package)
+minutes_used = int(input("How many minutes did you use this month? "))
+package_price = 0.0
+
+if package == "A" or package == "a":
     package_price = 39.99
-if minutes_used > 450
-    additional_minutes + minutes_used - 450
+    if minutes_used > 450:
+        additional_minutes = minutes_used - 450
+        minutes_cost = additional_minutes * .45
+        total_cost = package_price + minutes_cost
+        print("With package " + package + " and " + str(minutes_used) + " minutes used, you owe: $" + format(total_cost, ',.2f'))
+    else:
+        print("With package " + package + " and " + str(minutes_used) + " minutes used, you owe: $" + format(package_price, '.2f'))
+elif package == "B" or package == "b":
+    package_price = 59.99
+    additional_minutes = minutes_used - 900
+    minutes_cost = additional_minutes + .40
+    total_cost = package_price + minutes_cost
+    print("With package " + package + " and " + str(minutes_used) + " minutes used, you owe: $" + format(total_cost, '2.f'))
+
+elif package == "C" or package == "c":
+    package_price = 69.99
+    print("With package " + package + " you owe: $" + format(package_price, ' .2f'))
+else:
+    print("That is not a valid package.")
