f18ec9eebce7a6e14c0e3fa255516a3f16766285
echedgy <35992036+echedgy@users.noreply.github.com>
2019-09-12 13:50:52 -0500

    Add files via upload

---
+++ 4.3 Average Rainfall.py
@@ -0,0 +1,10 @@
+monthly_rainfall = 0
+Total_rainfall = 0
+num_years = 0
+average_rainfall = 0
+
+num_years = int(input("ask user for number of years"))
+for num in range(num_years):
+    print(num + 1)
+    for month in ("January, February, March, April, May, June, July, August, September, October, November, December"):
+        
