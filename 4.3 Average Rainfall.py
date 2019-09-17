3ad66313224c4de1bbb00a1c278caa6ba1462d7c
echedgy <35992036+echedgy@users.noreply.github.com>
2019-09-17 01:05:20 -0500

    Add files via upload

--- 4.3 Average Rainfall.py
+++ 4.3 Average Rainfall.py
@@ -1,10 +1,15 @@
-monthly_rainfall = 0
-Total_rainfall = 0
-num_years = 0
-average_rainfall = 0
+num_months = 0
+inches_of_rain = 0
+num_years = int(input("how many years would you like to collect data for?"))
 
-num_years = int(input("ask user for number of years"))
-for num in range(num_years):
-    print(num + 1)
-    for month in ("January, February, March, April, May, June, July, August, September, October, November, December"):
-        
+print("This program will calculate total rainfall and average monthly rainfall for a period of years.")
+for current_year in range(1, num_years + 1):
+    for current_month in range(1, 13):
+        monthly_rain_inches = float(input("Type the inches of rain in month" + format(current_month, "d") + ", year" + format(current_year, "d") + ": "))
+        inches_of_rain += monthly_rain_inches
+        num_months += 1
+
+avg_rainfall = inches_of_rain / num_months
+
+print("Your number of months:" + format( num_months, "d"), "Total" + " inches of rain: " + format(inches_of_rain, ".2f"), "average of rain: " + format( avg_rainfall, ".2f"), sep="\n")
+
