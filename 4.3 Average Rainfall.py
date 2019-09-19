494bdc663bd5c702468a9c9beb41efab39d09a16
echedgy <35992036+echedgy@users.noreply.github.com>
2019-09-19 01:01:35 -0500

    Add files via upload

--- 4.3 Average Rainfall.py
+++ 4.3 Average Rainfall.py
@@ -1,15 +1,18 @@
-num_months = 0
-inches_of_rain = 0
-num_years = int(input("how many years would you like to collect data for?"))
+grand_total = 0
 
 print("This program will calculate total rainfall and average monthly rainfall for a period of years.")
-for current_year in range(1, num_years + 1):
-    for current_month in range(1, 13):
-        monthly_rain_inches = float(input("Type the inches of rain in month" + format(current_month, "d") + ", year" + format(current_year, "d") + ": "))
-        inches_of_rain += monthly_rain_inches
-        num_months += 1
 
-avg_rainfall = inches_of_rain / num_months
+num_year = int(input("how many years would you like to collect data for?"))
+num_month = num_year * 12
 
-print("Your number of months:" + format( num_months, "d"), "Total" + " inches of rain: " + format(inches_of_rain, ".2f"), "average of rain: " + format( avg_rainfall, ".2f"), sep="\n")
+for year in range(num_year):
+    total = 0
+    print('Year ', year + 1)
+    for month in ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"):
+        current_rain = int(input('How much rain fell during the month of ' + month + "? "))
+        total += current_rain
+        grand_total += current_rain
+    print('The total amount of rainfall was:', total)
+    print('The average monthly rainfall was:', total / 12)
 
+print("For " + str(num_year) + " years there was a total of " + format(grand_total, ', .2f') + " inches of rain. There was an average of " + format(grand_total / (num_year * 12)) + " inches of rain per month.")
