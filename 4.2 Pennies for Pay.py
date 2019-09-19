6712f7da1098bbb2c633bb24a08564e36cd7ec31
echedgy <35992036+echedgy@users.noreply.github.com>
2019-09-18 23:29:39 -0500

    Add files via upload

--- 4.2 Pennies for Pay.py
+++ 4.2 Pennies for Pay.py
@@ -1,12 +1,18 @@
-num_Days_Worked = int( input( "How many days have you worked?" ))
-num_of_Pennies = 0
+total_earned = 0.01
+SALARY = 0.01
+daily_income = 0  # accumulator
 
-print ( "Day\tSalary\n----\t------" )
-for current_Day in range ( num_Days_Worked ):
-    pennies_For_Day = 2 ** current_Day
-    num_of_Pennies += pennies_For_Day
-    print( current_Day + 1, "\t" , pennies_For_Day )
-total_pay = num_of_Pennies * 0.01
+num_days = int(input("How many days have you worked?"))
+# day += 1
+print("Days Worked | Amount Earned That Day")
+for day in range(1, (num_days + 1)):
 
-print ( "\nTotal Pay: $", total_pay, sep= "")
+    if day == 1:
+        daily_income = SALARY
 
+    else:
+        daily_income *= 2
+        total_earned += daily_income
+    print(format(day, "5,d") + " | $" + format(daily_income, "30,.2f"))
+
+print("Total earned over " + str(num_days) + " days is $", format(total_earned, " ,.2f"))
