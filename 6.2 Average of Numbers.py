80a3e378d0b8f87163e553b3726c2f55193b82a9
echedgy <35992036+echedgy@users.noreply.github.com>
2019-10-01 16:48:15 -0500

    Add files via upload

--- 6.2 Average of Numbers.py
+++ 6.2 Average of Numbers.py
@@ -1,8 +1,31 @@
-def mian():
+def main():
+    input_file = open('numbers.txt', 'r')
+
+    try:
+        count = 1
+        record = input_file.readline()
+        record = record.rstrip('\n')
+        record = int(record)
+        total = record
+        print("line count is: " + str(count))
+        print("total value of records is: " + format(total, ','))
+        print("Average of records is: " + format(total, ",.2f"))
+
+        while record != '':
+            record = input_file.readline()
+            record = record.rstrip('\n')
+            record = int(record)
+            count += 1
+            total += record
+            average = total / count
+            print("line count is: " + str(count))
+            print("total value of records is: " + format(total, ','))
+            print("Average of records is: " + format(total, ",.2f"))
+
+    except ValueError:
+        print("error: Something is wrong")
+
+        input_file.close()
 
 
-    input_file = open('numbers(1).txt' 'r')
-
-    record = input_file.readline()
-    record = record.rstrip("\n")
-while record != "":
+main()
