eab5fb9791c1561db453a9243fa60629fd6e7191
echedgy <35992036+echedgy@users.noreply.github.com>
2019-10-03 03:45:05 -0500

    Add files via upload

--- chapter 6 exercises.py
+++ chapter 6 exercises.py
@@ -23,41 +23,46 @@ state_capitals = open('capitals.txt' 'w')
 #    There is a list of state capitals here: https://en.wikipedia.org/wiki/List_of_capitals_in_the_United_States
 # Sample:
 #   state_capitals.write("Alabama, Montgomery\n") - make sure to include the \n as a new line symbol
-
+state_capitals.write("Hawaii, Honolulu\n")
+state_capitals.write("Delaware, Dover\n")
+state_capitals.write("Indiana, Indianapolis\n")
 
 # 5) Close the file
 
-
+state_capitals.close()
 # TODO 6.1 reading data in from a file
 print("=" * 10, "Section 6.1 reading data from a file", "=" * 10)
 # 1) Assign the variable states_data to open states.txt in read mode
-
+states_data = open('states.txt' 'r')
 # 2) Read in three lines from the file, assign to the variables below, Remove """   """ to test
-"""
-line1 =
-line2 =
-line3 =
-"""
+
+line1 = states_data.readline()
+line2 = states_data.readline()
+line3 = states_data.readline()
+
 
 # 3) Close the file
-
+states_data.close()
 # 4) Print the three variables
-
+print(line1 + "\n" + line2 + "\n" + line3)
 # TODO 6.2 Using loops to process files
 print("=" * 10, "Section 6.2 use loops to process files", "=" * 10)
 # Complete the program below to Read in and Count all of the entries in the states file
 # Remove the """ """ before testing
 # 1) Open the file in read mode using states_file as the file variable
-"""
-states_file =
+
+states_file = open('states.txt', 'r')
 counter = 0
-"""
+
 # 2) Write a for loop to read in all of the lines,
 # -- print them on the screen,
 # -- and add 1 to counter for each line
-
+for line in states_file:
+    print(line)
+    counter += 1
 # 3) Close the file
-
+states_file.close()
+print('count =', counter)
 # TODO 6.3 Processing records
 print("=" * 10, "Section 6.3 processing records", "=" * 10)
 # You are going to finish the program below to write book information to a file
@@ -65,24 +70,27 @@ books = 3      # use this as the number of books to enter
 
 # 1) open the file books.txt for writing, using the variable books_file
 # Remove the """ """ to test
-"""books_file = """
+books_file = open('books.txt', 'w')
 
 # 2) Use a for loop to get a title and author from the user using the range 1, books + 1
 # -- get the data from the user in the loop
 # -- write the data to the file as a record while in the loop,
 #    make sure to include the \n at the end of the line
-
+for count in range(1, books + 1):
+    print('Enter in book data')
+    book = input("Please enter the book's table and author: ")
 # 3) Close the file
-
+books_file.close()
 # TODO 6.4 Exceptions
 print("=" * 10, "Section 6.4 exceptions", "=" * 10)
 # In this exercise you will try to open a file that does not exist,
 # capture the error, and display a custom error message
 
 # 1) Create a try statement
-
-# 2) Open the file superheros.txt for READING (we are not writing, it would create the file)
-
+try:
+    infile = open("superheroes.txt", "r")
 # 3) Close the file
-
+    infile.close()
 # 4) Create an except IOError, that uses a print statement telling the user that the file doesn't exist
+except IOError:
+    print("That file does not exist")
