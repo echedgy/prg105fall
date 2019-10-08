16f9e89adb4351b324cd4859fcf8e2a270f17c09
echedgy <35992036+echedgy@users.noreply.github.com>
2019-10-08 02:40:52 -0500

    Add files via upload

--- 7.1 Rainfall Statistics.py
+++ 7.1 Rainfall Statistics.py
@@ -6,7 +6,7 @@ print("This program will calculate total rainfall and average monthly rainfall f
 monthly_rainfall = 0
 total_rainfall = 0
 total_rainfall += monthly_rainfall
-average_rainfall = total_rainfall / 12
+
 total = 0
 
 for month in ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"):
@@ -16,5 +16,3 @@ for month in ("January", "February", "March", "April", "May", "June", "July", "A
     grand_total += current_rain
     print('The total amount of rainfall was:', total)
     print('The average monthly rainfall was:', total / 12)
-
-
