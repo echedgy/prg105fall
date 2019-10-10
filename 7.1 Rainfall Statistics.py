7040d078c642a640d639be73eb633edb63b187b4
echedgy <35992036+echedgy@users.noreply.github.com>
2019-10-10 10:43:29 -0500

    Add files via upload

--- 7.1 Rainfall Statistics.py
+++ 7.1 Rainfall Statistics.py
@@ -1,18 +1,23 @@
-grand_total = 0
+def main():
 
-print("This program will calculate total rainfall and average monthly rainfall for a period of years.")
+    monthly_rainfall = []
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    total_rainfall - 0
 
-
-monthly_rainfall = 0
-total_rainfall = 0
-total_rainfall += monthly_rainfall
-
-total = 0
-
-for month in ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"):
-    current_rain = int(input('How much rain fell during the month of ' + month + "? "))
-    total += current_rain
-    average_rainfall = total / 5
-    grand_total += current_rain
-    print('The total amount of rainfall was:', total)
-    print('The average monthly rainfall was:', total / 12)
+for month in months:
+    rainfall = float(input("Enter rainfall total for the month of " + month + " "))
+    monthly_rainfall.append(rainfall)
+    total_rainfall += rainfall
+# Data processing
+    average_rainfall = total_rainfall / 12
+    min_rainfall = min(monthly_rainfall)
+    max_rainfall = max(monthly_rainfall)
+    index_min = monthly_rainfall.index(min_rainfall)
+    index_max = monthly_rainfall.index(max_rainfall)
+# output
+    print("The total rainfall for the year was " + str(total_rainfall))
+    print("The avereage rainfall for the year was " + format(average_rainfall, ",.2f"))
+    print("The minimum amount of rainfall for the month was " + str(min_rainfall) + " inches."))
+    print("The month with the minimum rainfall was " + months[index_min]))
+    print("The maximum amount of rainfall for a month was " + str(max_rainfall) + " inch(es)")
+    print("The month with the maximum rainfall was " + months[index_max])
