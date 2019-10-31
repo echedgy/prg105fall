d7bf64f57d37a3402a828426a266270ee74cb4e9
echedgy <35992036+echedgy@users.noreply.github.com>
2019-10-31 02:09:13 -0500

    Add files via upload

--- 10.1 Personal Information Class.py
+++ 10.1 Personal Information Class.py
@@ -7,24 +7,24 @@
 # Print the data from each object, make sure to format the output for clarity and ease of reading.
 
 
-class Personal:
-    def __init__(self, name, address, age, phone):
-        self.__name = name
-        self.__address = address
-        self.__age = age
-        self.__phone = phone
+class Person:
+    def __init__(self, name_input, address_input, age_input, phone_input):
+        self.__name = name_input
+        self.__address = address_input
+        self.__age = age_input
+        self.__phone = phone_input
 
-    def set_name(self, name):
-        self.__name = name
+    def set_name(self, name_input):
+        self.__name = name_input
 
-    def set_address(self, address):
-        self.__address = address
+    def set_address(self, address_input):
+        self.__address = address_input
 
-    def set_age(self, age):
-        self.__age = age
+    def set_age(self, age_input):
+        self.__age = age_input
 
-    def set_phone(self, phone):
-        self.__phone(self, phone)
+    def set_phone(self, phone_input):
+        self.__phone(self, phone_input)
 
     def get_name(self):
         return self.__name
@@ -37,3 +37,20 @@ class Personal:
 
     def get_phone(self):
         return self.__phone
+    def __str__(self):
+        return 'Name: ' + self.__name + "\Address " + self.__address +\
+            "\nAge " + self.__age + "\nPhone: " + self.__phone
+
+
+def main():
+    person1 = Person("Eric", "4713 Southhampton Dr. Island Lake", "25", "847-271-9987")
+    person2 = Person("Matt", "102 Roxbury Lane, Noblesville", "25", "773-202-LUNA")
+    person3 = Person("Pete", "4613 Southhampton Dr. island Lake", "26", "847-921-1551")
+    print(person1)
+    print()
+    print(person2)
+    print()
+    print(person3)
+
+
+main()
